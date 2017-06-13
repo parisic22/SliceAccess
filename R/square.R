efdc021afb69415fdbc0338f8232d53c8fc701c0
parisic22 <christian@slicelife.com>
2017-06-13 15:03:03 -0400

    f

--- R/square.R
+++ R/square.R
@@ -1,9 +1,10 @@
 require(RPostgreSQL)
+# use load_all to get a description file to fill in.
 # use roxygen to build help file
 #' Square
 #' @author cp
 #' @param x a number to square
-#' @export #needed for other users to use
+#' @export #needed for other users to use.  if not exported is not available
 
 
 square <- function(x){
