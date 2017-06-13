963370fc4d4f0b749973457ce5202bce7a4c4fd4
parisic22 <christian@slicelife.com>
2017-06-13 11:18:53 -0400

    test

---
+++ R/connect.R
@@ -0,0 +1,6 @@
+require(RPostgreSQL)
+
+connect <- function(){
+  con_seg <-  dbConnect(PostgreSQL(), dbname='analytics', host = "veritas.c3byxptpwzdt.us-east-1.redshift.amazonaws.com", port=5439, user = "mypizza", password = "ji9P329nso9u3joinfcjd034")
+  con_seg
+}
