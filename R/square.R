92036dd13c7ea54cdfd985866c5f1b0a88a5b7e8
parisic22 <christian@slicelife.com>
2017-06-13 14:47:42 -0400

    a

--- R/square.R
+++ R/square.R
@@ -1,4 +1,10 @@
 require(RPostgreSQL)
+# use roxygen to build help file
+#' Square
+#' @author cp
+#' @param x a number to square
+#' @export #needed for other users to use
+
 
 square <- function(x){
   return(x^2)
